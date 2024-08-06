033728ebfbe892a33b78c38c51913aac60fef74c
ianmmworia <ianmworia156@gmail.com>
2024-08-06 23:15:52 +0300

    easy styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -166,13 +166,14 @@ function Home() {
 
       <div className="flex min-h-screen items-center justify-center">
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h1 className="mb-4 text-2xl font-bold text-blue-700">
+          <h1 className="mb-4 text-2xl font-bold text-center text-blue-700">
             Welcome, {user.name}!
           </h1>
           {user.role === "lawyer" ? (
             <p>You can manage your clients and provide legal services.</p>
           ) : (
-            <p>You can find and hire legal experts.</p>
+            <p> Thank you for choosing us to be your partner in your journey to find credible and reliable legal representation! <br/>
+            <em className="font-bold">Here you can find and hire legal experts.</em></p>
           )}
         </div>
       </div>
