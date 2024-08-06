a282246a2e58d920f70980d83213dcd575ba413f
ianmmworia <ianmworia156@gmail.com>
2024-08-06 22:01:10 +0300

    few changes here and there

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,55 +1,136 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    role: 'client',
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    role: "client",
     qualificationCertificate: null,
-    yearsOfExperience: '',
-    ratePerHour: ''
+    yearsOfExperience: "",
+    ratePerHour: "",
   });
 
   const handleChange = (e) => {
     const { name, value, type, files } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: type === 'file' ? files[0] : value
+      [name]: type === "file" ? files[0] : value,
     }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
-    navigate('/');
+    navigate("/");
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <form onSubmit={handleSubmit} className="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
         <h2 className="mb-6 text-2xl font-bold text-center">Sign Up</h2>
-        
+
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-            Name
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            First Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="firstname"
+            type="text"
+            name="firstname"
+            value={formData.firstname}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Last Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="lastname"
+            type="text"
+            name="lastname"
+            value={formData.lastname}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            ID No.
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="idno"
+            type="text"
+            name="idno"
+            value={formData.idno}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Phone No.
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="name"
             type="text"
-            name="name"
-            value={formData.name}
+            name="phone"
+            value={formData.phone}
             onChange={handleChange}
             required
           />
         </div>
-        
+
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Area of Residence
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            name="area"
+            value={formData.area}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
             Email
           </label>
           <input
@@ -62,9 +143,12 @@ function SignUp() {
             required
           />
         </div>
-        
+
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
             Password
           </label>
           <input
@@ -77,24 +161,12 @@ function SignUp() {
             required
           />
         </div>
-        
+
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
-            Confirm Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="confirmPassword"
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="role"
+          >
             Role
           </label>
           <select
@@ -108,25 +180,14 @@ function SignUp() {
             <option value="lawyer">Lawyer</option>
           </select>
         </div>
-        
-        {formData.role === 'lawyer' && (
+
+        {formData.role === "lawyer" && (
           <>
             <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="qualificationCertificate">
-                Qualification Certificate
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="qualificationCertificate"
-                type="file"
-                name="qualificationCertificate"
-                onChange={handleChange}
-                required
-              />
-            </div>
-            
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="yearsOfExperience">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="yearsOfExperience"
+              >
                 Years of Experience
               </label>
               <input
@@ -139,9 +200,45 @@ function SignUp() {
                 required
               />
             </div>
-            
+
             <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="ratePerHour">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="specialization">
+                Specialization
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="specialization"
+                type="number"
+                name="specialization"
+                value={formData.specialization}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="ratePerHour"
+              >
+                Image
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="image"
+                type="file"
+                name="image"
+                value={formData.image}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="ratePerHour"
+              >
                 Rate/Hour
               </label>
               <input
@@ -154,9 +251,27 @@ function SignUp() {
                 required
               />
             </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="qualificationCertificate"
+              >
+                Qualification Certificate
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="qualificationCertificate"
+                type="file"
+                name="qualificationCertificate"
+                onChange={handleChange}
+                required
+              />
+            </div>
+
           </>
         )}
-        
+
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
