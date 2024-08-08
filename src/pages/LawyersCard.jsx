57ddb7cd764396188742fa2c5332512d5a88829c
Leslie Mwangi <luciannahphoi@gmail.com>
2024-08-08 11:57:36 +0300

    color

--- src/pages/LawyersCard.jsx
+++ src/pages/LawyersCard.jsx
@@ -27,7 +27,7 @@ const LawyersCard = ({ lawyer }) => {
             background: '#e8e8e8',
           }}
         />
-        <h5 className="mb-1 text-xl font-medium text-white-900 dark:text-white">
+        <h5 className="mb-1 text-xl font-medium text-white dark:text-white">
           Name: {name}
         </h5>
         <span className="text-xl text-white dark:text-gray-400">
