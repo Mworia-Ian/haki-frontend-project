5c659ee198ee0450496b9d828a15b5ff054d295c
Phil <phil.github@gmail.com>
2024-08-09 13:04:37 +0300

    installed zod

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import SignUp from './pages/Signup';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Landing from './pages/Landing';
-import LawyerProfile from './pages/LawyerProfile';
-import LawyersGrid from './pages/LawyersGrid';
-import Chat from './pages/Chat';
-import Footer from './components/Footer';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SignUpForm from "./components/SignUpForm";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
+import LawyerProfile from "./pages/LawyerProfile";
+import LawyersGrid from "./pages/LawyersGrid";
+import Chat from "./pages/Chat";
+import Footer from "./components/Footer";
 
 function App() {
   return (
