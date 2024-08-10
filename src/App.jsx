8cbc21d5ea35d0f537d22922eaca21afa6af6650
Phil <phil.github@gmail.com>
2024-08-10 12:34:30 +0300

    added login route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Welcome from "./components/Welcome";
 import Landing from "./pages/Landing";
 import LawyerProfile from "./pages/LawyerProfile";
 import LawyersGrid from "./pages/LawyersGrid";
+import Login from "./pages/Login";
 import ClientChat from "./pages/ClientChat";
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <div className="min-h-screen bg-gray-100">
         <Routes>
           <Route path="/signup" element={<SignUpForm />} />
-
+          <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/" element={<Landing />} />
