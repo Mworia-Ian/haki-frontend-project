bce9b4095fbed668f9f833f2cbeeee73ae65ef61
ianmmworia <ianmworia156@gmail.com>
2024-08-16 12:12:30 +0300

    fix server url

--- utils.js
+++ utils.js
@@ -1,2 +1,2 @@
 export const SERVER_URL =
-  process.env.NODE_ENV === "development" ? "https://haki-backend.onrender.com" : process.env.SERVER_URL;
+  process.env.NODE_ENV === "development" ? "http://127.0.0.1:5000" : "https://haki-backend.onrender.com";
