50477771158a5b541e3220ff2bdebf21eb473b52
Phil <phil.github@gmail.com>
2024-08-19 23:59:24 +0300

    toggle fixed

--- src/pages/CasesCard.jsx
+++ src/pages/CasesCard.jsx
@@ -56,7 +56,7 @@ function CasesCard() {
 
   const handleLogout = () => {
     setUser(null);
-    localStorage.removeItem('session');
+    localStorage.removeItem("session");
     navigate("/login");
   };
 
@@ -98,7 +98,7 @@ function CasesCard() {
         Back
       </button>
       <button
-        onClick={() => navigate('/cases/add')}
+        onClick={() => navigate("/cases/add")}
         className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-[#37B9F1] rounded-lg hover:bg-[#32a6d8] focus:ring-4 focus:outline-none focus:ring-blue-300 mb-3 ml-4"
       >
         Add Case
@@ -116,12 +116,12 @@ function CasesCard() {
                     type="checkbox"
                     value=""
                     className="sr-only peer"
-                    checked={activeToggleId === card.id}
-                    onChange={() => handleToggle(card.id)}
+                    checked={activeToggleId === index}
+                    onChange={() => handleToggle(index)}
                   />
                   <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#32a6d8]"></div>
                   <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    {activeToggleId === card.id ? "Active" : "Inactive"}
+                    {activeToggleId === index ? "Active" : "Inactive"}
                   </span>
                 </label>
               </div>
