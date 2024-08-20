10a9f85227a71e730e7b0c6a00e960b0f61f4571
ianmmworia <ianmworia156@gmail.com>
2024-08-20 06:17:46 +0300

    removed one card

--- src/components/Landing/Card.jsx
+++ src/components/Landing/Card.jsx
@@ -6,7 +6,7 @@ function Card() {
   const navigate = useNavigate();
   return (
     <div id="Plans" className="flex items-center w-full py-[6rem] px-4 bg-[#F2F5F5] mb-5">
-      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 gap-8'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols gap-8'>
         <div className="w-full shadow-xl bg-[#37B9F1] text-[#242d2d] flex flex-col p-12 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-20 mx-auto mt-[3rem] bg-transparent"
@@ -19,24 +19,6 @@ function Card() {
             <p className="py-2 border-b mx-8 mt-8">Unrestricted Access</p>
             <p className="py-2 border-b mx-8">Direct Contact with Lawyers</p>
             <p className="py-2 border- mx-8">24hr Support</p>
-          </div>
-          <button onClick={() => navigate("/signup")} className="bg-[#242d2d] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-[#37B9F1] hover:bg-[#CBD4F4] hover:text-[#00636D]">
-          Get Started
-        </button>
-        </div>
-
-        <div className="w-full shadow-xl bg-[#37B9F1] text-[#242d2d] flex flex-col p-12 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
-          <img
-            className="w-20 mx-auto mt-[3rem] bg-transparent"
-            src="/assets/reshot-icon-justice-XH4D3B8RWF.svg"
-            alt="/"
-          />
-          <h2 className="text-2xl font-bold text-center py-8 text-[#242d2d]">Membership</h2>
-          <p className="text-center text-4xl font-bold">Free</p>
-          <div className="text-center font-medium">
-            <p className="py-2 border-b mx-8 mt-8">Restricted Access</p>
-            <p className="py-2 border-b mx-8">No Direct Contact with Lawyers</p>
-            <p className="py-2 border- mx-8">4hr Support</p>
           </div>
           <button onClick={() => navigate("/signup")} className="bg-[#242d2d] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-[#37B9F1] hover:bg-[#CBD4F4] hover:text-[#00636D]">
           Get Started
